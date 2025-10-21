adb6fb32ae79891c552809276e4227ada7afd096
paulophsj <snoopyphs@gmail.com>
2025-10-21 16:07:22 -0300

    Adicionando rotas e validação de body.json()

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
 import { configDotenv } from "dotenv";
 import express from "express";
 import cors from "cors"
-import sequelize from "./config/db.js";
+import sequelize from "./config/db.config.js";
+import db from "./models/index.model.js";
+
+import { createRoles } from "./models/role.model.js";
+
+import authRouter from "./routers/auth.router.js"
+import empresaRouter from "./routers/empresa.router.js"
+import adminRouter from "./routers/admin.router.js"
+import chatRouter from "./routers/chat.router.js"
+
+import { authenticationMiddleware } from "./middlewares/authentication.middleware.js";
+import roleMiddleware from "./middlewares/role.middleware.js";
 
 configDotenv()
 
 const app = express()
+
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
 
 app.use(cors({
     allowedHeaders: ["Content-Type", "Auhtorization"],
@@ -13,10 +27,14 @@ app.use(cors({
     origin: "http://localhost:3000"
 }))
 
-
+app.use("/api/admin", adminRouter) //Proteger essa rota em produção
+app.use("/api/auth", authRouter)
+app.use("/api/empresa", authenticationMiddleware, roleMiddleware.canEmpresa, empresaRouter)
+app.use("/api/chat", authenticationMiddleware, roleMiddleware.canEmpresaAndFuncionario, chatRouter)
 
 try {
   await sequelize.sync({alter: true})
+  await createRoles()
   console.log('Banco de dados conectado com sucesso!');
 } catch (error) {
   console.error('Erro ao conetar ao banco de dados:', error);
