422a7295783a7e5347c7392f70b2a34df881a476
paulophsj <snoopyphs@gmail.com>
2025-10-24 15:43:31 -0300

    Adicionando novo endpoint para verificar status da autenticação

--- routers/auth.router.js
+++ routers/auth.router.js
@@ -1,7 +1,9 @@
 import {Router} from "express"
 import authController from "../controllers/auth.controller.js"
+import { authenticationMiddleware } from "../middlewares/authentication.middleware.js"
 const router = Router()
 
+router.get("/check", authenticationMiddleware, authController.checkAuth)
 router.post("/login/empresa", authController.logarEmpresa)
 router.post("/login/funcionario", authController.logarFuncionario)
 
