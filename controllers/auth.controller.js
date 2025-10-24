d476f5369e0b69e6bee9a05c78692715f9051cf3
paulophsj <snoopyphs@gmail.com>
2025-10-24 15:43:06 -0300

    Refatorando retorno da API da sessão e adicionando um novo endpoint para verificar o status da autenticação

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,22 +1,34 @@
 import authService from "../services/auth.service.js";
 
 export default {
-    logarEmpresa: async (req,res) => {
+    logarEmpresa: async (req, res) => {
         try {
-            const {email, password} = req.body
+            const { email, password } = req.body
             const token = await authService.logarEmpresa(email, password)
-            return res.status(201).json({token})
+            return res.status(201).json({ token })
         } catch (error) {
-            return res.status(error.status || 500).json({message: error.message || "Erro interno do servidor."})
+            return res.status(error.status || 500).json({ message: error.message || "Erro interno do servidor." })
         }
     },
-    logarFuncionario: async (req,res) => {
-      try {
-            const {email, password, newPassword} = req.body
+    logarFuncionario: async (req, res) => {
+        try {
+            const { email, password, newPassword } = req.body
             const token = await authService.logarFuncionario(email, password, newPassword)
-            return res.status(201).json({token})
+            return res.status(201).json({ token })
         } catch (error) {
-            return res.status(error.status || 500).json({message: error.message || "Erro interno do servidor."})
-        }  
+            return res.status(error.status || 500).json({ message: error.message || "Erro interno do servidor." })
+        }
+    },
+    checkAuth: async (req, res) => {
+        try {
+            const headers = req.headers
+            const token = headers?.authorization?.split("Bearer ")[1]
+
+            const decoded = await authService.checkAuth(token)
+            
+            return res.status(200).json(decoded)
+        } catch (error) {
+            return res.status(500).json({message: "Erro interno do servidor"})
+        }
     }
 }
--- controllers/session.controller.js
+++ controllers/session.controller.js
@@ -4,23 +4,22 @@ import wppStatus from "../wpp/wpp.status.js"
 export default {
     criarSession: async (req, res) => {
         try {
-            const {nome: nomeEmpresa} = req.user
+            const { nome: nomeEmpresa } = req.user
 
             const session = await sessionService.criarSession(nomeEmpresa)
             return res.status(201).json(session.dataValues)
         } catch (error) {
-            return res.status(error.status || 500).json({message: error.message || "Erro interno do servidor"})
+            return res.status(error.status || 500).json({ message: error.message || "Erro interno do servidor" })
         }
     },
-    iniciarSessao: async (req,res) => {
+    iniciarSessao: async (req, res) => {
         try {
-            const {id: empresa_id, nome: nomeEmpresa} = req.user
-            const data = await wppStatus.iniciarSessao(empresa_id, nomeEmpresa)
-            setTimeout(() => {
-                return res.status(200).json({data})
-            }, 3000);
+            const { id: empresa_id, nome: nomeEmpresa } = req.user
+            const qrCode = await wppStatus.iniciarSessao(empresa_id, nomeEmpresa)
+            const status = qrCode == null ? "pending" : "ok"
+            return res.status(200).json({status, qrCode})
         } catch (error) {
-            return res.status(error.status || 500).json({message: error.message || "Erro interno do servidor."})
+            return res.status(error.status || 500).json({ message: error.message || "Erro interno do servidor." })
         }
     }
 }
