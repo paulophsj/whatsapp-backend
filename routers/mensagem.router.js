ae4a4419dbb65f4566d6f138f4c1448dfb9e8125
paulophsj <snoopyphs@gmail.com>
2025-11-01 17:03:23 -0300

    Adicionando rota para buscar a primeira mensagem

--- routers/mensagem.router.js
+++ routers/mensagem.router.js
@@ -4,5 +4,6 @@ import mensagemController from "../controllers/mensagem.controller.js"
 const router = Router()
 
 router.get("/:id", mensagemController.findAllMensagensByChatId)
+router.get("/primeira-mensagem/:id", mensagemController.findFirstMessage)
 
 export default router
