3513bd389967f5e5bb31595a73b4cd428de55e42
paulophsj <snoopyphs@gmail.com>
2025-11-01 17:02:02 -0300

    Adicionando busca de primeira mensagem do cliente para estatisticas

--- controllers/mensagem.controller.js
+++ controllers/mensagem.controller.js
@@ -11,5 +11,16 @@ export default {
         } catch (error) {
             return res.status(error.status || 500).json({message: error.message || "Erro interno do servidor."})
         }
+    },
+    findFirstMessage: async (req,res) => {
+        try {
+            const {id: cliente_id} = req.params
+
+            const mensagens = await mensagemService.findFirstMessage(cliente_id)
+            
+            return res.status(200).json(mensagens)
+        } catch (error) {
+            return res.status(error.status || 500).json({message: error.message || "Erro interno do servidor."})
+        }
     }
 }
