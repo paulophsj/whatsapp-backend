d5ca48c9484d74603af058f29e33972ce5201571
paulophsj <snoopyphs@gmail.com>
2025-11-06 15:19:32 -0300

    Criando o service do commit anterior

--- services/chat.service.js
+++ services/chat.service.js
@@ -1,7 +1,9 @@
+import { Op } from "sequelize"
 import Chat from "../models/chat.model.js"
 import Cliente from "../models/cliente.model.js"
 import Estatisticas from "../models/estatisticas.model.js"
 import { validatePhone } from "../utils/regex.util.js"
+import Funcionario from "../models/funcionario.model.js"
 
 export default {
     criarChat: async (numeroCliente, funcionario_id) => {
@@ -12,7 +14,7 @@ export default {
             throw {status: 400, message: "O campo numeroCliente não pode ser vazio e deve ser válido"}
         }
 
-        const cliente = await Cliente.findOne({where: {numeroCliente}})
+        const cliente = await Cliente.findOne({where: {numeroCliente, ativo: true}})
 
         if(!cliente){
             throw {status: 404, message: "Cliente não encontrado na base de dados"}
@@ -51,9 +53,64 @@ export default {
     },
     todosClientesSemAtendimento: async (empresa_id) => {
         const clientes = await Cliente.findAll({
-            where: {empresa_id, emAtendimento: false}
+            where: {empresa_id, emAtendimento: false, ativo: true}
         })
 
         return clientes
+    },
+    buscarChatsFinalizados: async (funcionario_id) => {
+        if(!funcionario_id){
+            throw {status: 400, message: "Todos os campos são obrigatórios."}
+        }
+
+        const chats = await Chat.findAll({
+            where: {
+                isActive: false,
+                funcionario_id,
+                updatedAt: {[Op.between]: [new Date().toISOString().split("T")[0].concat("T00:00:00"), new Date().toISOString().split("T")[0].concat("T23:59:59")]}
+            },
+            include: [
+                {
+                    model: Cliente,
+                    attributes: ["id", "nome"]
+                },
+                {
+                    model: Funcionario,
+                    attributes: ["id", "nome"]
+                }
+            ]
+        })
+
+        return chats
+    },
+    finalizarChat: async (chat_id, funcionario_id) => {
+        if(!chat_id){
+            throw {status: 400, message: "O campo chat_id é obrigatório"}
+        }
+        if(!funcionario_id){
+            throw {status: 400, message: "O campo funcionario_id é obrigatório"}
+        }
+
+        const chat = await Chat.findByPk(chat_id)
+
+        if(!chat){
+            throw {status: 400, message: "Chat não encontrado"}
+        }
+        if(!chat.isActive){
+            throw {status: 400, message: "Este chat já foi finalizado."}
+        }
+        if(chat.funcionario_id !== funcionario_id){
+            throw {status: 400, message: "Você não tem permissão para finalizar este chat"}
+        }
+
+        const cliente = await Cliente.findByPk(chat.cliente_id)
+
+        await cliente.update({
+            ativo: false,
+            emAtendimento: false
+        })
+        await chat.update({
+            isActive: false
+        })
     }
 }
