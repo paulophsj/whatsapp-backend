54fe605e1fa380ed3e4126a01e5fc81837a901d6
paulophsj <snoopyphs@gmail.com>
2025-11-06 14:54:22 -0300

    Alterando o timeZone para Brasil

--- config/db.config.js
+++ config/db.config.js
@@ -6,6 +6,7 @@ configDotenv()
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
   host: process.env.DB_HOST,
   dialect: process.env.DB_TYPE,
+  timezone: '-03:00',
   define: {
     freezeTableName: true
   },
